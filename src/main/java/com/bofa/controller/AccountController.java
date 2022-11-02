79cf5a0ba15d30725a1019a3f5b32a97da9fcafc
rostyslavmoskal <moskalrostyslav@gmail.com>
2022-11-01 19:38:54 -0700

    feature: add account validation

--- src/main/java/com/bofa/controller/AccountController.java
+++ src/main/java/com/bofa/controller/AccountController.java
@@ -6,10 +6,12 @@ import com.bofa.repository.AccountRepository;
 import com.bofa.service.AccountService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Date;
 import java.util.UUID;
 
@@ -36,7 +38,11 @@ public class AccountController {
     }
 
     @PostMapping("/create")
-    public String createAccount(Account account) {
+    public String createAccount(@Valid Account account, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("accountTypes", AccountType.values());
+            return "account/create-account";
+        }
         accountService.createNewAccount(account.getBalance(), new Date(), account.getAccountType(), account.getUserId());
 
         return "redirect:/index";
--- src/main/java/com/bofa/model/Account.java
+++ src/main/java/com/bofa/model/Account.java
@@ -7,6 +7,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.UUID;
@@ -15,9 +17,13 @@ import java.util.UUID;
 @Builder
 public class Account {
     private UUID id;
+    @NotNull
+    @PositiveOrZero
     private BigDecimal balance;
+    @NotNull
     private AccountType accountType;
     private Date creationDate;
+    @NotNull
     private Long userId;
     private AccountStatus accountStatus;
 }
