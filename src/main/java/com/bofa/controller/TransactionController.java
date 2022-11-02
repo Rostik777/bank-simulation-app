ca7610bd5b96207584a02e253a5b6b6eb13a78d1
rostyslavmoskal <moskalrostyslav@gmail.com>
2022-11-01 20:47:19 -0700

    feature: transfer validation added

--- src/main/java/com/bofa/controller/TransactionController.java
+++ src/main/java/com/bofa/controller/TransactionController.java
@@ -7,11 +7,13 @@ import com.bofa.service.TransactionService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Date;
 import java.util.UUID;
 
@@ -36,7 +38,12 @@ public class TransactionController {
     }
 
     @PostMapping("/transfer")
-    public String postMakeTransfer(@ModelAttribute("transaction") Transaction transaction) {
+    public String postMakeTransfer(@Valid @ModelAttribute("transaction") Transaction transaction, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("accounts", accountService.listAllAccounts());
+            return "/transaction/make-transfer";
+        }
+
         Account sender = accountService.retrieveById(transaction.getSender());
         Account receiver = accountService.retrieveById(transaction.getReceiver());
         transactionService.makeTransfer(sender, receiver, transaction.getAmount(), new Date(), transaction.getMessage());
